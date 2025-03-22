6b618bc9df7aac6214b33457300c7747d8a1e1ca
Nadishi <159514648+NadishiChristina@users.noreply.github.com>
2025-03-22 09:10:02 +0530

    Update AchievePlusBeApplication.java

--- AchievePlusBeApplication.java
+++ AchievePlusBeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AchievePlusBeApplication {
 
-    public static void main(String[] args) {
+    public static main(String[] args) {
         SpringApplication.run(AchievePlusBeApplication.class, args);
     }
 
